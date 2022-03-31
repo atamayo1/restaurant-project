391b181fbadc78e3d83a48acd5ab3a7f616c894b
Anthony Tamayo Ortega <anthonytamayoortega@MacBook-Pro-de-Anthony.local>
2022-03-31 11:55:34 -0500

    i add better restaurants

--- restaurant-app/src/components/RestaurantDetail/RestaurantDetail.jsx
+++ restaurant-app/src/components/RestaurantDetail/RestaurantDetail.jsx
@@ -10,10 +10,10 @@ const RestaurantDetail = (props) => {
         <div className="container">
           <div className="row">
             <div className="col-6">
-              <img style={{maxWidth: '100%',}} src={restaurant.image} alt="Logo de la Conferencia" />
+              <img style={{maxWidth: '100%', maxHeight: '11em',}} src={restaurant?.image} alt="Logo de la Conferencia" />
             </div>
             <div className="col-6 RestaurantDetails__brand-attendant-name">
-              <h1>{restaurant.name}</h1>
+              <h1>{restaurant?.name}</h1>
             </div>
           </div>
         </div>
@@ -24,19 +24,21 @@ const RestaurantDetail = (props) => {
           <div className="col">
             <div className="Restaurant">
               <div className="Restaurant__header">
-                <img src={restaurant.image} alt="Logo de la conferencia" />
+                <img src={restaurant?.image} alt="Logo de la conferencia" />
               </div>
 
               <div className="Restaurant__section-name">
-                <h1>{restaurant.name}</h1>
+                <h1>{restaurant?.name}</h1>
               </div>
 
               <div className="Restaurant__section-info">
-                <h3>Address: {restaurant.address}</h3>
-                <div>Phone: {restaurant.phone}</div>
-                <div>Email: {restaurant.email}</div>
-                <div>Website: {restaurant.website}</div>
-                <div>Reviews: {restaurant.reviews}</div>
+                <h3>Address: {restaurant?.address}</h3>
+                <div>Neighborhood: {restaurant?.neighborhood}</div>
+                <div>Cuisine Type: {restaurant?.cuisine_type}</div>
+                <div>Phone: {restaurant?.phone}</div>
+                <div>Email: {restaurant?.email}</div>
+                <div>Website: {restaurant?.website}</div>
+                <div>Reviews: {restaurant?.reviews}</div>
               </div>
             </div>
           </div>
